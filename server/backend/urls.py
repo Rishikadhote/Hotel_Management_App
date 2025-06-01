6bc8251d8445dffd7f35977cc6de2d179d681fda
Rishika <rishikadhote99@gmail.com>
2025-06-01 14:19:28 +0530

    root update

--- server/backend/urls.py
+++ server/backend/urls.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def root_api_view(request):
+    return JsonResponse({
+        "status": "success",
+        "message": "Welcome to the Hotel Management API 🚀",
+        "available_routes": [
+            "/api/rooms/",
+            "/api/customers/",
+            "/api/bookings/",
+            "/api/contact/",
+            "/api/booking/"
+        ]
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('hotel.urls')),
-]
+    path('', root_api_view),
+ ]
